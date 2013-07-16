372ac0719f1dcde23cf654a4b56f4c633c39a78e
Divij Bindlish <dvjbndlsh93@gmail.com>
2013-07-16 18:27:50 +0530

    Changed indentation style

--- js/script.js
+++ js/script.js
@@ -1,239 +1,239 @@
 $(document).ready(function(){
 
-	//code for showing sign up and login forms
-	$('#sign').click(function(){
-		$('#login_container').fadeOut('fast');
-		$('#regis_container').fadeIn('fast');
-		$('#auth_container').css("height","50%");
-	});
-	$('#log').click(function(){
-		$('#login_container').fadeIn('fast');
-		$('#regis_container').fadeOut('fast');
-		$('#auth_container').css("height","40%")
-	});
+  //code for showing sign up and login forms
+  $('#sign').click(function(){
+    $('#login_container').fadeOut('fast');
+    $('#regis_container').fadeIn('fast');
+    $('#auth_container').css("height","50%");
+  });
+  $('#log').click(function(){
+    $('#login_container').fadeIn('fast');
+    $('#regis_container').fadeOut('fast');
+    $('#auth_container').css("height","40%")
+  });
 
-	//code for footer
-	$('.links').mouseenter(function(){
-		var id = $(this).attr('id');
-		if(id=="ab"){
-			$('#about').fadeIn('fast');
-		}
-		if(id=="con"){
-			$('#contact').fadeIn('fast');
-		}
-		if(id=="copy"){
-			$('#copyright').fadeIn('fast');
-		}
-		$('.links').mouseleave(function(){
-			$('.footer_notification').fadeOut('fast');
-		});
-	});
+  //code for footer
+  $('.links').mouseenter(function(){
+    var id = $(this).attr('id');
+    if(id=="ab"){
+      $('#about').fadeIn('fast');
+    }
+    if(id=="con"){
+      $('#contact').fadeIn('fast');
+    }
+    if(id=="copy"){
+      $('#copyright').fadeIn('fast');
+    }
+    $('.links').mouseleave(function(){
+      $('.footer_notification').fadeOut('fast');
+    });
+  });
 
-	//functions to check whether ticks in login and register forms are visible
-	function check_login(){
-		if($('#login_form>#tick1').css('display')=='inline' && $('#login_form>#tick2').css('display')=='inline'){
-			$('#login_submit').removeClass('disabled');
-		}
-		else{
-			$('#login_submit').addClass('disabled');
-		}
-		if($('#uname_login').val()==""||$("#pass_login").val()==""){
-			$('#login_submit').addClass('disabled');
-		}
-	}
-	function check_register(){
-		if($('#register_form>#tick1').css('display')=='inline' && $('#register_form>#tick2').css('display')=='inline' && $('#register_form>#tick3').css('display')=='inline' && $('#register_form>#tick4').css('display')=='inline' && $('#register_form>#tick5').css('display')=='inline'){
-			$('#regis_submit').removeClass('disabled');
-		}
-		else{
-			$('#regis_submit').addClass('disabled');
-			if($('#uname_regis').val()==""){
-				$("#regis_submit").addClass('disabled');
-			}
-		}
-	}
+  //functions to check whether ticks in login and register forms are visible
+  function check_login(){
+    if($('#login_form>#tick1').css('display')=='inline' && $('#login_form>#tick2').css('display')=='inline'){
+      $('#login_submit').removeClass('disabled');
+    }
+    else{
+      $('#login_submit').addClass('disabled');
+    }
+    if($('#uname_login').val()==""||$("#pass_login").val()==""){
+      $('#login_submit').addClass('disabled');
+    }
+  }
+  function check_register(){
+    if($('#register_form>#tick1').css('display')=='inline' && $('#register_form>#tick2').css('display')=='inline' && $('#register_form>#tick3').css('display')=='inline' && $('#register_form>#tick4').css('display')=='inline' && $('#register_form>#tick5').css('display')=='inline'){
+      $('#regis_submit').removeClass('disabled');
+    }
+    else{
+      $('#regis_submit').addClass('disabled');
+      if($('#uname_regis').val()==""){
+        $("#regis_submit").addClass('disabled');
+      }
+    }
+  }
 
 
 
-	/*
-	 *
-	Code for validating login data begins
-	 *
-	 */
+  /*
+   *
+  Code for validating login data begins
+   *
+   */
 
-	//Code for checking password
-	$('#pass_login').on("keyup",function(){
-		var pass = $(this).val();
-		if(pass.length<6){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Password too short</p>');
-			$('#login_form #cross2').fadeIn('fast');
-			$('#login_form #tick2').fadeOut('fast');
-			$('#login_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#login_form #cross2').fadeOut('fast');
-			$('#login_form #tick2').fadeIn('fast');
-		}
-		check_login();
-	});
+  //Code for checking password
+  $('#pass_login').on("keyup",function(){
+    var pass = $(this).val();
+    if(pass.length<6){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Password too short</p>');
+      $('#login_form #cross2').fadeIn('fast');
+      $('#login_form #tick2').fadeOut('fast');
+      $('#login_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#login_form #cross2').fadeOut('fast');
+      $('#login_form #tick2').fadeIn('fast');
+    }
+    check_login();
+  });
 
-	//Code for checking username
-	$('#uname_login').on("keyup",function(){
-		var uname = $(this).val();
-		if(uname.length<5){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Username too short</p>');
-			$('#login_form #cross1').fadeIn('fast');
-			$('#login_form #tick1').fadeOut('fast');
-			$('#login_submit').addClass('disabled');
-		}
-		else if(/^[a-zA-Z0-9]*$/.test(uname) == false){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html("<p>Username contains illegal characters<p>");
-			$('#login_form #cross1').fadeIn('fast');
-			$('#login_form #tick1').fadeOut('fast');
-			$('#login_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#login_form #cross1').fadeOut('fast');
-			$('#login_form #tick1').fadeIn('fast');
-		}
-		check_login();
-	});
+  //Code for checking username
+  $('#uname_login').on("keyup",function(){
+    var uname = $(this).val();
+    if(uname.length<5){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Username too short</p>');
+      $('#login_form #cross1').fadeIn('fast');
+      $('#login_form #tick1').fadeOut('fast');
+      $('#login_submit').addClass('disabled');
+    }
+    else if(/^[a-zA-Z0-9]*$/.test(uname) == false){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html("<p>Username contains illegal characters<p>");
+      $('#login_form #cross1').fadeIn('fast');
+      $('#login_form #tick1').fadeOut('fast');
+      $('#login_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#login_form #cross1').fadeOut('fast');
+      $('#login_form #tick1').fadeIn('fast');
+    }
+    check_login();
+  });
 
-	/*
-	 *
-	 Code for validating registeration data begins
-	 *
-	 */
+  /*
+   *
+   Code for validating registeration data begins
+   *
+   */
 
-	//Code for checking name of user
-	$('#name_regis').on("keyup",function(){
-		var name = $(this).val();
-		if(name.length<4){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Name too short</p>');
-			$('#register_form>#cross1').fadeIn('fast');
-			$('#register_form>#tick1').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else if(/^[a-zA-Z0-9 ]*$/.test(name) == false){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html("<p>Name contains illegal characters<p>");
-			$('#register_form #cross1').fadeIn('fast');
-			$('#register_form #tick1').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#register_form #cross1').fadeOut('fast');
-			$('#register_form #tick1').fadeIn('fast');
-		}
-		check_register();
-	});
+  //Code for checking name of user
+  $('#name_regis').on("keyup",function(){
+    var name = $(this).val();
+    if(name.length<4){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Name too short</p>');
+      $('#register_form>#cross1').fadeIn('fast');
+      $('#register_form>#tick1').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else if(/^[a-zA-Z0-9 ]*$/.test(name) == false){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html("<p>Name contains illegal characters<p>");
+      $('#register_form #cross1').fadeIn('fast');
+      $('#register_form #tick1').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#register_form #cross1').fadeOut('fast');
+      $('#register_form #tick1').fadeIn('fast');
+    }
+    check_register();
+  });
 
-	//Code for checking username
-	$('#uname_regis').on("keyup",function(){
-		var uname = $(this).val();
-		if(uname.length<5){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Username too short</p>');
-			$('#register_form #cross2').fadeIn('fast');
-			$('#register_form #tick2').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else if(/^[a-zA-Z0-9]*$/.test(uname) == false){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html("<p>Username contains illegal characters<p>");
-			$('#register_form #cross2').fadeIn('fast');
-			$('#register_form #tick2').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#register_form #cross2').fadeOut('fast');
-			$('#register_form #tick2').fadeIn('fast');
-		}
-		check_register();
-	});
+  //Code for checking username
+  $('#uname_regis').on("keyup",function(){
+    var uname = $(this).val();
+    if(uname.length<5){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Username too short</p>');
+      $('#register_form #cross2').fadeIn('fast');
+      $('#register_form #tick2').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else if(/^[a-zA-Z0-9]*$/.test(uname) == false){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html("<p>Username contains illegal characters<p>");
+      $('#register_form #cross2').fadeIn('fast');
+      $('#register_form #tick2').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#register_form #cross2').fadeOut('fast');
+      $('#register_form #tick2').fadeIn('fast');
+    }
+    check_register();
+  });
 
-	//Code for checking email of a person
-	$('#email_regis').on("keyup",function(){
-		var email = $(this).val();
-		if(email.length<5){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Email too short</p>');
-			$('#register_form #cross3').fadeIn('fast');
-			$('#register_form #tick3').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else if(/^[a-zA-Z0-9]+[@]+[a-zA-Z0-9]+[.]+[a-z]*$/.test(email) == false){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html("<p>Email not in proper format<p>");
-			$('#register_form #cross3').fadeIn('fast');
-			$('#register_form #tick3').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#register_form #cross3').fadeOut('fast');
-			$('#register_form #tick3').fadeIn('fast');
-		}
-		check_register();
-	});	
-	
-	//Code for checking password
-	$('#pass_regis').on("keyup",function(){
-		var pass = $(this).val();
-		if(pass.length<6){
-			$('#auth_notification').fadeIn('fast');
-			$('#auth_notification').html('<p>Password too short</p>');
-			$('#register_form #cross4').fadeIn('fast');
-			$('#register_form #tick4').fadeOut('fast');
-			$('#regis_submit').addClass('disabled');
-		}
-		else{
-			$('#auth_notification').fadeOut('fast');
-			$('#register_form #cross4').fadeOut('fast');
-			$('#register_form #tick4').fadeIn('fast');
-		}
-		check_register();
-	});
+  //Code for checking email of a person
+  $('#email_regis').on("keyup",function(){
+    var email = $(this).val();
+    if(email.length<5){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Email too short</p>');
+      $('#register_form #cross3').fadeIn('fast');
+      $('#register_form #tick3').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else if(/^[a-zA-Z0-9]+[@]+[a-zA-Z0-9]+[.]+[a-z]*$/.test(email) == false){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html("<p>Email not in proper format<p>");
+      $('#register_form #cross3').fadeIn('fast');
+      $('#register_form #tick3').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#register_form #cross3').fadeOut('fast');
+      $('#register_form #tick3').fadeIn('fast');
+    }
+    check_register();
+  });
 
-	//Code for confirming passwords
-	$('#confirm_regis').on("keyup",function(){
-		var conf = $(this).val();
-		var orig = $('#pass_regis').val();
-		if(conf==orig && conf.length>0){
-			$('#register_form>#tick5').fadeIn('fast');
-			$('#register_form>#cross5').fadeOut('fast');
-			$('#auth_notification').fadeOut('fast');
-		}
-		else{
-			$('#register_form>#cross5').fadeIn('fast');
-			$('#register_form>#tick5').fadeOut('fast');
-			$("#auth_notification").html("<p>Both the passwords do not match</p>");
-			$('#auth_notification').fadeIn('fast');
-		}
-		check_register();
-	});
+  //Code for checking password
+  $('#pass_regis').on("keyup",function(){
+    var pass = $(this).val();
+    if(pass.length<6){
+      $('#auth_notification').fadeIn('fast');
+      $('#auth_notification').html('<p>Password too short</p>');
+      $('#register_form #cross4').fadeIn('fast');
+      $('#register_form #tick4').fadeOut('fast');
+      $('#regis_submit').addClass('disabled');
+    }
+    else{
+      $('#auth_notification').fadeOut('fast');
+      $('#register_form #cross4').fadeOut('fast');
+      $('#register_form #tick4').fadeIn('fast');
+    }
+    check_register();
+  });
 
-	/*
-	 *
-	 Code for validating add data begins
-	 *
-	 */
-	 $('#task_field').on("keyup",function(){
-	   var task = $(this).val();
-	   if(task!=""){
-	     $('#task_submit').removeClass('disabled');
-	   }
-	   else{
-	     $('#task_submit').addClass('disabled');
-	   }
-	 });
+  //Code for confirming passwords
+  $('#confirm_regis').on("keyup",function(){
+    var conf = $(this).val();
+    var orig = $('#pass_regis').val();
+    if(conf==orig && conf.length>0){
+      $('#register_form>#tick5').fadeIn('fast');
+      $('#register_form>#cross5').fadeOut('fast');
+      $('#auth_notification').fadeOut('fast');
+    }
+    else{
+      $('#register_form>#cross5').fadeIn('fast');
+      $('#register_form>#tick5').fadeOut('fast');
+      $("#auth_notification").html("<p>Both the passwords do not match</p>");
+      $('#auth_notification').fadeIn('fast');
+    }
+    check_register();
+  });
+
+  /*
+   *
+   Code for validating add data begins
+   *
+   */
+   $('#task_field').on("keyup",function(){
+     var task = $(this).val();
+     if(task!=""){
+       $('#task_submit').removeClass('disabled');
+     }
+     else{
+       $('#task_submit').addClass('disabled');
+     }
+   });
 
 });
--- todo.php
+++ todo.php
@@ -1,130 +1,130 @@
 <?php
 
-	//start the session
-	session_start();
+  //start the session
+  session_start();
     $config = include './config/config.php';
 
-	//create connection with mysql database
-	$con = mysql_connect($config['DB_HOST'],$config['DB_USER'],$config['DB_PASS']);
+  //create connection with mysql database
+  $con = mysql_connect($config['DB_HOST'],$config['DB_USER'],$config['DB_PASS']);
 
-	//check whether connection is made
-	if (!$con){
-  	die("Failed to connect to MySQL: " . mysql_error());
+  //check whether connection is made
+  if (!$con){
+    die("Failed to connect to MySQL: " . mysql_error());
   }
 
   //use database todo_list
   mysql_select_db($config['DB_NAME']);
 
   //create a table for storing information about all the users
-	$sql = "CREATE TABLE users(name VARCHAR(30), uname VARCHAR(30), email VARCHAR(30), pwd VARCHAR(30));";
-	mysql_query($sql,$con);
+  $sql = "CREATE TABLE users(name VARCHAR(30), uname VARCHAR(30), email VARCHAR(30), pwd VARCHAR(30));";
+  mysql_query($sql,$con);
 
-	//function to return all the data in the sql table
-	function getData($name){
-		global $con;
-		$sql = "SELECT * FROM ".$name.";";
-		$check = mysql_query($sql,$con);
-		if(!$check) return "";
-		$data="";
-		while($row = mysql_fetch_array($check,MYSQL_ASSOC)){
-			$no = $row['serialno'];
-			$content = $row['list1item'];
-			$imp = "<div id='".$no."' class='list_item'><form method='POST' action='index.php'><input type='submit' value='&#10006' class='remove_button hidden' name='remove_button btn' id='".$no."btn'/></form><input type='checkbox' class='check' id='".$no."cb'/><span>".$content."</span></div>";
-			$data=$data.$imp."\n";
-		}
-		return $data;
-	}
+  //function to return all the data in the sql table
+  function getData($name){
+    global $con;
+    $sql = "SELECT * FROM ".$name.";";
+    $check = mysql_query($sql,$con);
+    if(!$check) return "";
+    $data="";
+    while($row = mysql_fetch_array($check,MYSQL_ASSOC)){
+      $no = $row['serialno'];
+      $content = $row['list1item'];
+      $imp = "<div id='".$no."' class='list_item'><form method='POST' action='index.php'><input type='submit' value='&#10006' class='remove_button hidden' name='remove_button btn' id='".$no."btn'/></form><input type='checkbox' class='check' id='".$no."cb'/><span>".$content."</span></div>";
+      $data=$data.$imp."\n";
+    }
+    return $data;
+  }
 
-	/*
-		Code for using cookies begins
-	*/
-	if(isset($_POST['usingcookie'])){
-		$uname = $_POST['username'];
-		$data = getData($uname);
-		echo "success".$data;
-	}
+  /*
+    Code for using cookies begins
+  */
+  if(isset($_POST['usingcookie'])){
+    $uname = $_POST['username'];
+    $data = getData($uname);
+    echo "success".$data;
+  }
 
-	/*
-		Code for registering new user begins
-	*/
-	if(isset($_POST['regis_submit'])){
+  /*
+    Code for registering new user begins
+  */
+  if(isset($_POST['regis_submit'])){
 
-		//get all form data using for each loop
-		$data = "";
-		foreach($_POST as $key => $value){
-			if($key!="regis_submit"){
-				$data=$data.",'".$value."'";
-			}
-		}
-		$data=substr($data,1);
+    //get all form data using for each loop
+    $data = "";
+    foreach($_POST as $key => $value){
+      if($key!="regis_submit"){
+        $data=$data.",'".$value."'";
+      }
+    }
+    $data=substr($data,1);
 
-		//create new record in table users
-		$sql = "INSERT INTO users VALUES (".$data.");";
-		if(mysql_query($sql,$con)){
-			$_SESSION["user"]=$_POST["uname_regis"];
-			$sql = "CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno));";
-			mysql_query($sql,$con);
-			echo "success";
-		}
-		else echo "fail";
-	}
+    //create new record in table users
+    $sql = "INSERT INTO users VALUES (".$data.");";
+    if(mysql_query($sql,$con)){
+      $_SESSION["user"]=$_POST["uname_regis"];
+      $sql = "CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno));";
+      mysql_query($sql,$con);
+      echo "success";
+    }
+    else echo "fail";
+  }
 
-	/*
-		Code for Login Form Begins
-	*/
-	if(isset($_POST['login_submit'])){
-		$uname = $_POST['uname_login'];
-		$sql = "SELECT pwd FROM users WHERE uname = '$uname';";
-		$check = mysql_query($sql,$con);
-		$row = mysql_fetch_array($check,MYSQL_ASSOC);
-		if($row){
-			$pass = $row['pwd'];
-		}
-		if($_POST['pass_login']==$pass){
-			$_SESSION["user"]=$uname;
-			echo "success".getData($uname);
-		}
-		else{
-			echo "fail";
-		}
-	}
+  /*
+    Code for Login Form Begins
+  */
+  if(isset($_POST['login_submit'])){
+    $uname = $_POST['uname_login'];
+    $sql = "SELECT pwd FROM users WHERE uname = '$uname';";
+    $check = mysql_query($sql,$con);
+    $row = mysql_fetch_array($check,MYSQL_ASSOC);
+    if($row){
+      $pass = $row['pwd'];
+    }
+    if($_POST['pass_login']==$pass){
+      $_SESSION["user"]=$uname;
+      echo "success".getData($uname);
+    }
+    else{
+      echo "fail";
+    }
+  }
 
-	/*
-		Code for adding new list item begins
-	*/
-	if(isset($_POST['task_submit'])){
-		$item = $_POST['task_field'];
-		$uname = $_SESSION["user"];
-		$sql = "INSERT INTO ".$_SESSION["user"]."(list1item) VALUES ('".$item."');";
-		$check = mysql_query($sql,$con);
-		if($check){
-			echo "success".getData($uname);
-		}
-		else{
-			echo "fail";
-		}
-	}
+  /*
+    Code for adding new list item begins
+  */
+  if(isset($_POST['task_submit'])){
+    $item = $_POST['task_field'];
+    $uname = $_SESSION["user"];
+    $sql = "INSERT INTO ".$_SESSION["user"]."(list1item) VALUES ('".$item."');";
+    $check = mysql_query($sql,$con);
+    if($check){
+      echo "success".getData($uname);
+    }
+    else{
+      echo "fail";
+    }
+  }
 
-	/*
-		Code for removing an item begins
-	*/
-	if(isset($_POST["remove_button"])){
-		$no = $_POST["id"];
-		$sql = "DELETE FROM ".$_SESSION["user"]." WHERE serialno='".$no."';";
-		if(mysql_query($sql,$con)){
-			echo "success";
-		}
-		else{
-			echo "fail";
-		}
-	}
+  /*
+    Code for removing an item begins
+  */
+  if(isset($_POST["remove_button"])){
+    $no = $_POST["id"];
+    $sql = "DELETE FROM ".$_SESSION["user"]." WHERE serialno='".$no."';";
+    if(mysql_query($sql,$con)){
+      echo "success";
+    }
+    else{
+      echo "fail";
+    }
+  }
 
-	/*
-		Code for logout begins
-	*/
-	if(isset($_POST["logout_submit"])){
-		session_destroy();
-		session_start();
-	}
+  /*
+    Code for logout begins
+  */
+  if(isset($_POST["logout_submit"])){
+    session_destroy();
+    session_start();
+  }
 
 ?>
