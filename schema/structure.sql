d73b25d019bb24aef8852e9663801efee1705ec6
Divij Bindlish <dvjbndlsh93@gmail.com>
2013-07-17 00:39:37 +0530

    Added sql schema. Changed to PDO for database queries.

---
+++ schema/structure.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS 'users' (
+  'name' varchar(30) NOT NULL,
+  'uname' varchar(30) NOT NULL,
+  'email' varchar(30) NOT NULL,
+  'pwd' varchar(30) NOT NULL
+);
--- todo.php
+++ todo.php
@@ -1,47 +1,25 @@
 <?php
-
-  //start the session
-  session_start();
+    session_start();
     $config = include './config/config.php';
 
-  //create connection with mysql database
-  $con = mysql_connect($config['DB_HOST'],$config['DB_USER'],$config['DB_PASS']);
-
-  //check whether connection is made
-  if (!$con){
-    die("Failed to connect to MySQL: " . mysql_error());
-  }
-
-  //use database todo_list
-  mysql_select_db($config['DB_NAME']);
-
-  //create a table for storing information about all the users
-  $sql = "CREATE TABLE users(name VARCHAR(30), uname VARCHAR(30), email VARCHAR(30), pwd VARCHAR(30));";
-  mysql_query($sql,$con);
+    $db = new PDO("mysql:host=".$config['DB_HOST'].";dbname=".$config['DB_NAME'], $config['DB_USER'], $config['DB_PASS']);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   //function to return all the data in the sql table
   function getData($name){
-    global $con;
-    $sql = "SELECT * FROM ".$name.";";
-    $check = mysql_query($sql,$con);
-    if(!$check) return "";
-    $data="";
-    while($row = mysql_fetch_array($check,MYSQL_ASSOC)){
-      $no = $row['serialno'];
-      $content = $row['list1item'];
-      $imp = "<div id='".$no."' class='list_item'><form method='POST' action='index.php'><input type='submit' value='&#10006' class='remove_button hidden' name='remove_button btn' id='".$no."btn'/></form><input type='checkbox' class='check' id='".$no."cb'/><span>".$content."</span></div>";
-      $data=$data.$imp."\n";
+    global $db;
+    $q = $db->query("SELECT * FROM $name;");
+    if(!$q)
+      return "";
+    $q->setFetchMode(PDO::FETCH_OBJ);
+    $data = "";
+    while($row = $q->fetch()) {
+      $no = $row->serialno;
+      $content = $row->list1item;
+      $imp = "<div id='$no' class='list_item'><form method='POST' action='index.php'><input type='submit' value='&#10006' class='remove_button hidden' name='remove_button btn' id='".$no."btn'/></form><input type='checkbox' class='check' id='".$no."cb'/><span>$content</span></div>";
+      $data = $data.$imp."\n";
     }
     return $data;
-  }
-
-  /*
-    Code for using cookies begins
-  */
-  if(isset($_POST['usingcookie'])){
-    $uname = $_POST['username'];
-    $data = getData($uname);
-    echo "success".$data;
   }
 
   /*
@@ -51,22 +29,20 @@
 
     //get all form data using for each loop
     $data = "";
-    foreach($_POST as $key => $value){
-      if($key!="regis_submit"){
-        $data=$data.",'".$value."'";
-      }
-    }
-    $data=substr($data,1);
+    foreach($_POST as $key => $value)
+      if($key!="regis_submit")
+        $data = "$data,'$value'";
+    $data = substr($data, 1);
 
     //create new record in table users
-    $sql = "INSERT INTO users VALUES (".$data.");";
-    if(mysql_query($sql,$con)){
-      $_SESSION["user"]=$_POST["uname_regis"];
-      $sql = "CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno));";
-      mysql_query($sql,$con);
+    $q = $db->query("INSERT INTO users VALUES ($data);");
+    if($q){
+      $db->query("CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno));");
       echo "success";
     }
-    else echo "fail";
+    else
+      echo "fail";
+
   }
 
   /*
@@ -74,19 +50,16 @@
   */
   if(isset($_POST['login_submit'])){
     $uname = $_POST['uname_login'];
-    $sql = "SELECT pwd FROM users WHERE uname = '$uname';";
-    $check = mysql_query($sql,$con);
-    $row = mysql_fetch_array($check,MYSQL_ASSOC);
-    if($row){
-      $pass = $row['pwd'];
-    }
-    if($_POST['pass_login']==$pass){
-      $_SESSION["user"]=$uname;
+    $q = $db->query("SELECT pwd FROM users WHERE uname = '$uname';");
+    $q->setFetchMode(PDO::FETCH_OBJ);
+    if($row = $q->fetch())
+      $pass = $row->pwd;
+    if($_POST['pass_login'] == $pass){
+      $_SESSION["user"] = $uname;
       echo "success".getData($uname);
     }
-    else{
+    else
       echo "fail";
-    }
   }
 
   /*
@@ -95,14 +68,11 @@
   if(isset($_POST['task_submit'])){
     $item = $_POST['task_field'];
     $uname = $_SESSION["user"];
-    $sql = "INSERT INTO ".$_SESSION["user"]."(list1item) VALUES ('".$item."');";
-    $check = mysql_query($sql,$con);
-    if($check){
+    $q = $db->query("INSERT INTO $uname(list1item) VALUES ('$item');");
+    if($q)
       echo "success".getData($uname);
-    }
-    else{
+    else
       echo "fail";
-    }
   }
 
   /*
@@ -110,21 +80,11 @@
   */
   if(isset($_POST["remove_button"])){
     $no = $_POST["id"];
-    $sql = "DELETE FROM ".$_SESSION["user"]." WHERE serialno='".$no."';";
-    if(mysql_query($sql,$con)){
+    $q = $db->query("DELETE FROM ".$_SESSION["user"]." WHERE serialno='$no';");
+    if($q)
       echo "success";
-    }
-    else{
+    else
       echo "fail";
-    }
-  }
-
-  /*
-    Code for logout begins
-  */
-  if(isset($_POST["logout_submit"])){
-    session_destroy();
-    session_start();
   }
 
 ?>
