d4ec0975f61c8f6f388ca3304b0af5b24c1181f4
Divij Bindlish <dvjbndlsh93@gmail.com>
2013-07-19 23:27:03 +0530

    Fixed some bugs

--- todo.php
+++ todo.php
@@ -8,12 +8,11 @@
   //function to return all the data in the sql table
   function getData($name){
     global $db;
-    $q = $db->query("SELECT * FROM $name;");
+    $q = $db->query("SELECT * FROM $name");
     if(!$q)
       return "";
-    $q->setFetchMode(PDO::FETCH_OBJ);
     $data = "";
-    while($row = $q->fetch()) {
+    while($row = $q->fetch(PDO::FETCH_OBJ)) {
       $no = $row->serialno;
       $content = $row->list1item;
       $imp = "<div id='$no' class='list_item'><form method='POST' action='index.php'><input type='submit' value='&#10006' class='remove_button hidden' name='remove_button btn' id='".$no."btn'/></form><input type='checkbox' class='check' id='".$no."cb'/><span>$content</span></div>";
@@ -35,9 +34,10 @@
     $data = substr($data, 1);
 
     //create new record in table users
-    $q = $db->query("INSERT INTO users VALUES ($data);");
+    $q = $db->query("INSERT INTO users VALUES ($data)");
     if($q){
-      $db->query("CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno));");
+      $db->query("CREATE TABLE ".$_POST['uname_regis']."(serialno INT NOT NULL AUTO_INCREMENT, list1item VARCHAR(30), PRIMARY KEY (serialno))");
+      $_SESSION["user"] = $_POST["uname_regis"]
       echo "success";
     }
     else
@@ -50,9 +50,9 @@
   */
   if(isset($_POST['login_submit'])){
     $uname = $_POST['uname_login'];
-    $q = $db->query("SELECT pwd FROM users WHERE uname = '$uname';");
-    $q->setFetchMode(PDO::FETCH_OBJ);
-    if($row = $q->fetch())
+    $q = $db->prepare("SELECT pwd FROM users WHERE uname = ?");
+    $q->execute(array($uname));
+    if($row = $q->fetch(PDO::FETCH_OBJ))
       $pass = $row->pwd;
     if($_POST['pass_login'] == $pass){
       $_SESSION["user"] = $uname;
@@ -68,7 +68,7 @@
   if(isset($_POST['task_submit'])){
     $item = $_POST['task_field'];
     $uname = $_SESSION["user"];
-    $q = $db->query("INSERT INTO $uname(list1item) VALUES ('$item');");
+    $q = $db->query("INSERT INTO $uname(list1item) VALUES ('$item')");
     if($q)
       echo "success".getData($uname);
     else
@@ -80,7 +80,7 @@
   */
   if(isset($_POST["remove_button"])){
     $no = $_POST["id"];
-    $q = $db->query("DELETE FROM ".$_SESSION["user"]." WHERE serialno='$no';");
+    $q = $db->query("DELETE FROM ".$_SESSION["user"]." WHERE serialno='$no'");
     if($q)
       echo "success";
     else
